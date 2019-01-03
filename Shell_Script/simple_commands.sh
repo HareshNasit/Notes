e4728de7affe119373abf935edb2fedd08e30c0e
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-01-02 20:21:42 -0500

    Updated notes for simple commands.

--- Shell_Script/simple_commands.sh
+++ Shell_Script/simple_commands.sh
@@ -1,3 +1,16 @@
 #Created by Haresh Nasit on 2/1/19
 #Copyright © 2019 Haresh Nasit. All rights reserved.
 #References: PCRS videos from CSC209 at University of Toronto
+
+# Shell script files are saved with the format .sh
+
+# FILE MANIPULATION COMMANDS
+
+# man command is used to get information about any command.
+# e.g. man ls
+# This will list the description and examples of how to use the given command.
+
+# ls command will list all the contents in the current directory.
+# For instance if the current directory contains a.sh, b.sh and c.sh,
+ls
+# This will list the contents as a.sh b.sh c.sh
