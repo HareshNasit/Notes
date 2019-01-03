4534ccff8ef4977a6f37af4de273b03fe4f372db
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-01-02 21:05:42 -0500

    Added more commands in simple_commands.sh file.

--- Shell_Script/simple_commands.sh
+++ Shell_Script/simple_commands.sh
@@ -4,14 +4,13 @@
 
 # Shell script files are saved with the format .sh
 # These commands can be run on the terminal
-# Note: test the below commands individually on the terminal.
-
-# FILE MANIPULATION COMMANDS
+# Test the below commands individually on the terminal.
 
 # man ____ command is used to get information about any command.
 # e.g.
 man ls
 # This will list the description and examples of how to use the given command.
+# It also displays the different arguements which are called flags that can be used.
 
 # ls command will list all the contents in the current directory.
 # For instance if the current directory contains a.sh, b.sh and c.sh,
@@ -33,7 +32,7 @@ cd ./Movies/Marvel
 # pwd is used to check the complete path of the working directory.
 # e.g.
 pwd
-# This prints Desktop/Movies/Marvel when run on the terminal.
+# This prints Users/harsh/Desktop/Movies/Marvel when run on the terminal for me.
 
 # cp _____ _____ is used to copy file or files specified as the first arguement
 # to the second arguement which is a directory.
@@ -48,3 +47,14 @@ rm notes
 rm *.sh
 # rm notes removes the notes directory and rm *.sh removes all the files that
 # end with .sh
+
+# cat _____ displays the contents of the file specified.
+# e.g. the current directory contains a.sh, file.c, file1.py
+cat a.sh # This displays the code for a.sh
+cat file.c # Displays the file file.c
+cat file1.py # Displays the code in file1.py
+
+# grep ____ ____ is used to print the lines in a given file(2nd arguement) that match the
+# string (1st arguement).
+# e.g. file days.txt contains all the days
+grep es days.txt # This will print Tuesday and Wednesday as only these 2 lines contain es.
