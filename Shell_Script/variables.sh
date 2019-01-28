d2972d0d046d3a0cea1b55e26b6cc45c2e9e9c27
Haresh Ashokbhai Nasit <harsh.nasit@mail.utoronto.ca>
2019-01-27 21:12:44 -0500

    Created new file on variables in shell.

---
+++ Shell_Script/variables.sh
@@ -0,0 +1,12 @@
+#Created by Haresh Nasit on 2/1/19
+#Copyright © 2019 Haresh Nasit. All rights reserved.
+#References: PCRS videos from CSC209 course at University of Toronto
+
+#To declare a variable, name the variable and assign the value.
+#There should be no spaces when assigning variables as shell will 
+# consider the defined variable as a command with arguements.
+i=3 #This assigns variable i to STRING 3.
+animal=cat
+#In shell, everything is declared as a string.
+
+
